c3deaf081628e1e10db08295c2c3a219e4c5f580
Michaël COLL <mick.coll@gmail.com>
2016-12-06 22:10:08 +0100

    Version update

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build --no-cache --rm -t michaelcoll/armhf-plex:1.2.7.2987 .
--- buildCache.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker build --rm -t michaelcoll/armhf-plex:1.2.7.2987 .
